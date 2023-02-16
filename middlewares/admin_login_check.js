a09382641be9483d10180e7bb65b9ad990f27f8b
=Mathew Alex <mathewalex557@gmail.com>
2023-02-16 12:11:56 +0530

    some new changes has been made

--- middlewares/admin_login_check.js
+++ middlewares/admin_login_check.js
@@ -1,21 +1,23 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-async-promise-executor */
-const admins = require("../models/admins")
+const admins = require("../models/admins");
 
 let adminModules = {
-    authCheck : (reqData)=>{
-        return new Promise( async(resolve,reject)=>{
-
-            let isUserLogged = false;
-            let userData = await admins.findOne({_id:reqData.session.adminid})
-            if(userData){
-                if(userData._id == reqData.session.adminid && userData.login_sess == reqData.session.login_sess_admin){
-                    isUserLogged = true;
-                }
-            }
-            resolve(isUserLogged)
-        })
-    }
-}
+  authCheck: (reqData) => {
+    return new Promise(async (resolve, reject) => {
+      let isUserLogged = false;
+      let userData = await admins.findOne({ _id: reqData.session.adminid });
+      if (userData) {
+        if (
+          userData._id == reqData.session.adminid &&
+          userData.login_sess == reqData.session.login_sess_admin
+        ) {
+          isUserLogged = true;
+        }
+      }
+      resolve(isUserLogged);
+    });
+  },
+};
 
 module.exports = adminModules;
