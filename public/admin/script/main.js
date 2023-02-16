ee46fcdac6565c7555b2f3fc2440bfa503e3e029
=Mathew Alex <mathewalex557@gmail.com>
2023-02-16 16:08:31 +0530

    image changed

--- public/admin/script/main.js
+++ public/admin/script/main.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 let toggleBTN = document.getElementById('toggleBTN');
 let sbar = document.getElementById('adsidebarForToggle');
 var togglesbar = ()=>{
